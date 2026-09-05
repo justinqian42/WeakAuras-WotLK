4228f05198d1053015f7feac00283b832edaa22a
NoM0Re <1629787+NoM0Re@users.noreply.github.com>
2026-09-05 22:40:50 +0200

    fix: preserve profiling data when renaming an aura

--- WeakAuras/Profiling.lua
+++ WeakAuras/Profiling.lua
@@ -193,7 +193,7 @@ local function StopProfileUID(uid)
 end
 
 function Private.ProfileRenameAura(oldid, id)
-  profileData.auras[id] = profileData.auras[id]
+  profileData.auras[id] = profileData.auras[oldid]
   profileData.auras[oldid] = nil
 end
 
